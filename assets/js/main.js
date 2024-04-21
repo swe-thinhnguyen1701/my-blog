3737de396bfc233ea3eaf580bb49a7410b4cd525
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-04-20 23:00:47 -0500

    modify mode icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 const modeSwitcher = document.getElementById("mode-switcher");
+const modeIcon = document.getElementById("mode-icon");
 
 const data = [];
 
@@ -8,8 +9,10 @@ modeSwitcher.addEventListener("click", function () {
     if(mode === "dark"){
         mode = "light";
         document.body.setAttribute("class", "light");
+        modeIcon.setAttribute("src", "/assets/img/light-mode.png")
     }else{
         mode = "dark";
         document.body.setAttribute("class", "dark");
+        modeIcon.setAttribute("src", "/assets/img/night-mode.png")
     }
 });
